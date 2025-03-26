83520fd5579c463ed12118d86a97bfda06bffb24
sanmathibs <sanmathi@blueskycreations.com.au>
2025-03-26 23:27:42 +1000

    added files

---
+++ app.py
@@ -0,0 +1,21 @@
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+
+app = FastAPI()
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
+templates = Jinja2Templates(directory="templates")
+
+@app.get("/", response_class=HTMLResponse)
+def read_root(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
+
+@app.get("/projects", response_class=HTMLResponse)
+def read_projects(request: Request):
+    return templates.TemplateResponse("projects.html", {"request": request})
+
+@app.get("/contact", response_class=HTMLResponse)
+def read_contact(request: Request):
+    return templates.TemplateResponse("contact.html", {"request": request})
